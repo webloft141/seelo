6f8e690d93dd220a55518f3fdc3a077a3016a941
webloft141 <webloft141@users.noreply.github.com>
2026-05-26 11:50:13 +0530

    fix: move MainActivity.kt to com.seelopro.app package

--- android/app/src/main/kotlin/com/seelopro/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/seelopro/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.seelo
+package com.seelopro.app
 
 import android.os.Build
 import android.os.Bundle
@@ -9,13 +9,11 @@ class MainActivity: FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         
-        // Ye flags notch aur status bar ke restrictions ko poori tarah khatam kar dete hain
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
             window.attributes.layoutInDisplayCutoutMode = 
                 WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
         }
         
-        // Sabse aggressive flag: Layout boundaries ko disable karna
         window.setFlags(
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
